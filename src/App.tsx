d0e3a9997e71a028230d0bcc90b16d86be2eb657
unknown <drafzaza123@icloud.com>
2023-09-12 19:53:13 +0700

    create user

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,22 @@ function App() {
       setUsers(originalUsers)
     })
   }
+  const addUser=()=>{
+    const originalUsers =[...users]
+    const newUser={id:0,name:'Draf'}
+    setUsers([newUser,...users])
+    axios.post('https://jsonplaceholder.typicode.com/users',newUser)
+    .then(({data:savedUser})=>setUsers([savedUser,...users]))
+    .catch(err=>{
+      setError(err.message)
+      setUsers(originalUsers)
+    })
+  }
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb3" onClick={addUser}>Add</button>
       <ul className="list-group">
         {users.map((user) => (
           <li key={user.id} className="list-group-item d-flex justify-content-between">
