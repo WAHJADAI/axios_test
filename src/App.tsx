2abc1677ff110992f92671c689ae27e363930430
unknown <drafzaza123@icloud.com>
2023-09-12 19:16:25 +0700

    useEffect clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import axios from 'axios'
+import "./App.css";
+import axios, { CanceledError } from "axios";
 
-interface User{
-  id:number,
-  name:string
+interface User {
+  id: number;
+  name: string;
 }
 function App() {
-  const [users, setUsers] = useState<User[]>([])
-  const [error,setError]= useState('')
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
-useEffect(()=>{
-  axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-  .then((res)=>setUsers(res.data))
-  .catch((err)=>setError(err.message))
-},[])
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
   return (
     <>
-      {error &&<p className="text-danger">{error}</p>}
+      {error && <p className="text-danger">{error}</p>}
       <ul>
-        {users.map((user)=><li key={user.id}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
